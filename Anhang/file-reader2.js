44d47beda281a51b728d054d5c65bda8277410f4
Philip Ackermann <scalarocker@googlemail.com>
2016-07-15 21:56:09 +0200

    Chapter 7

--- Anhang/file-reader2.js
+++
@@ -1,8 +0,0 @@
-var jf = require('jsonfile')
-var util = require('util')
-
-var file = 'HTML5.json'
-jf.readFile(file, function(err, obj) {
-  console.log(err);
-  console.log(util.inspect(obj))
-})
--- Kapitel7/7.2/001-Formulare-abschicken/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function init() {
-  let loginForm = document.getElementById('login');
-  let loginButton = document.getElementById('submit');
-  loginButton.addEventListener('click', (e) => {
-    loginForm.submit();
-  });
-}
-
-document.addEventListener('DOMContentLoaded', init);
