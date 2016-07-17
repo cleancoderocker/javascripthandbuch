519ffe3cdc3a15fbc809a1138e559ca4fd77be2a
Philip Ackermann <scalarocker@googlemail.com>
2016-07-17 22:57:16 +0200

    Chapter 17

--- Kapitel17/17.5/17.4.1/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-fs.readFile('input.txt', function (error, data) {
-  if (error) {
-    return console.error(error);
-  }
-  console.log(data.toString());
-});
--- Kapitel17/17.5/17.4.5/index.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-fs.stat('input.txt', function (error, stats) {
-  if (error) {
-    return console.error(error);
-  }
-  console.log(stats.isFile());        // true
-  console.log(stats.isDirectory());   // false
-  console.log(stats);                 // siehe nächstes Listing
-});
