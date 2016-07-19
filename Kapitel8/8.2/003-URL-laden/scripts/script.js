9c747e0b0e6b0efd61fc790ccf84c7b6873013d7
Philip Ackermann <scalarocker@googlemail.com>
2016-07-19 21:53:03 +0200

    Added chapter 8

--- Kapitel8/8.2/003-URL-laden/scripts/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-function init() {
-  let linkLoad = document.getElementById('link-load');
-  linkLoad.addEventListener('click', (e) => {
-    let url = document.getElementById('url').value;
-    window.location.href = url;
-  });
-}
-document.addEventListener('DOMContentLoaded', init)
