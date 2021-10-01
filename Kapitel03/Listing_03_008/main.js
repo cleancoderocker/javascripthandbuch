4e526c1a7fbb640dd9cf98d76f561b9da0d8fda9
Philip Ackermann <info@philipackermann.de>
2021-10-01 12:48:11 +0200

    Comment out examples for syntax errors

--- Kapitel03/Listing_03_008/main.js
+++ Kapitel03/Listing_03_008/main.js
@@ -1,7 +1,7 @@
 'use strict';
-const 2ndName = 'Moritz'; // ungültig, da mit Zahl beginnend
-const first%Name = 'Max'; // ungültig, da Sonderzeichen enthaltend
-const first-name = 'Max'; // ungültig, da Bindestrich enthaltend
+// const 2ndName = 'Moritz'; // ungültig, da mit Zahl beginnend
+// const first%Name = 'Max'; // ungültig, da Sonderzeichen enthaltend
+// const first-name = 'Max'; // ungültig, da Bindestrich enthaltend
 const first_name = 'Max'; // gültig
 const _firstName = 'Max'; // gültig
 const $firstName = 'Max'; // gültig
