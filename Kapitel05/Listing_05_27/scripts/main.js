3625c24a17c663170e3c75d212760318a7f1077f
Philip Ackermann <info@philipackermann.de>
2021-12-01 15:51:56 +0100

    Fix listing 5.27

--- Kapitel05/Listing_05_27/scripts/main.js
+++ Kapitel05/Listing_05_27/scripts/main.js
@@ -4,7 +4,7 @@ function init() {
     const innerHTML = document.querySelector('#news li:nth-child(1)').innerHTML;
     console.log(innerHTML);
     // Ausgabe: <strong>Platten-News: </strong>Neues Album von
-    // <em>Ben Harper</em> erschienen.
+    // <em>Tool</em> erschienen.
   })();
 
 }
