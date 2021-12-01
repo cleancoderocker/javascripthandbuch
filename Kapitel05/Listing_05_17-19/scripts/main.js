4b0a564986b8b7c47ca54cd6863fb3f0fbca5ef4
Philip Ackermann <info@philipackermann.de>
2021-12-01 15:51:47 +0100

    Fix listing 5.17 - 5.19

--- Kapitel05/Listing_05_17-19/scripts/main.js
+++ Kapitel05/Listing_05_17-19/scripts/main.js
@@ -10,14 +10,10 @@ function init() {
 
   (function() {
     const tbody = document.querySelector('tbody');
-    console.log(tbody.firstChild);  // Textknoten
-    console.log(tbody.lastChild);   // Textknoten
-  })();
-
-  (function() {
-    const tbody = document.querySelector('tbody');
-    console.log(tbody.firstElementChild);  // <tr>
-    console.log(tbody.lastElementChild);   // <tr>
+    console.log(tbody.firstChild);        // Textknoten
+    console.log(tbody.lastChild);         // Textknoten
+    console.log(tbody.firstElementChild); // <tr>
+    console.log(tbody.lastElementChild);  // <tr>
   })();
 
   (function() {
@@ -28,24 +24,6 @@ function init() {
     console.log(tableCell.nextElementSibling);     // <tr>
   })();
 
-  (function() {
-    const table = document.querySelector('table');
-    console.log(Object.getPrototypeOf(table));  // HTMLTableElement
-    console.log(table.caption);                 // null
-    console.log(table.tHead);                   // thead
-    console.log(table.tBodies);                 // [tbody]
-    console.log(table.rows);                    // [tr, tr.row.odd, tr.row.even, tr.row.odd, tr.row.even]
-    console.log(table.tFoot);                   // null
-
-    const newRow   = table.insertRow(1);
-    const newCellFirstName  = newRow.insertCell(0);
-    newCellFirstName.textContent = 'Bob';
-    const newCellLastName  = newRow.insertCell(1);
-    newCellLastName.textContent = 'Mustermann';
-    const newCellEmail  = newRow.insertCell(2);
-    newCellEmail.textContent = 'bob.mustermann@javascripthandbuch.de';
-
-  })();
 }
 
 document.addEventListener('DOMContentLoaded', init);
