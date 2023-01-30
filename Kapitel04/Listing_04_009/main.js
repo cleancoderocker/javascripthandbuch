acdc6c28c805219b7c497c41aa361c969e3c92c9
Philip Ackermann <info@philipackermann.de>
2023-01-30 15:11:44 +0100

    Update Listing 4.9

--- Kapitel04/Listing_04_009/main.js
+++ Kapitel04/Listing_04_009/main.js
@@ -1,13 +1,15 @@
 'use strict';
-const item = {
-  name: 'Schrödinger programmiert Java',
-  price: 44.90,
-  author: 'Philip Ackermann',
-  isbn: '978-3-8362-7272-8',
-  printDescription: function() {
+const array = [
+  ['name', 'Schrödinger programmiert Java'],
+  ['price', 44.90],
+  ['author', 'Philip Ackermann'],
+  ['isbn', '978-3-8362-7272-8'],
+  ['printDescription', function () {
     console.log(`${this.author}: ${this.name}`);
-  }
-}
+  }]
+];
+
+const item = Object.fromEntries(array);
 console.log(item.name);   // "Schrödinger programmiert Java"
 console.log(item.price);  // 44.90
 console.log(item.author); // "Philip Ackermann"
