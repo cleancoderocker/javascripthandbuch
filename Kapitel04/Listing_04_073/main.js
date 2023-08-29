a21bc9606b77cefa24edbb08b1da2886b08cee02
Philip Ackermann <info@philipackermann.de>
2023-08-29 10:47:23 +0200

    Added missing dot in listing 4.73

--- Kapitel04/Listing_04_073/main.js
+++ Kapitel04/Listing_04_073/main.js
@@ -4,5 +4,5 @@ const result = [2,3,4,5,6,7,2,3,4,5]
 console.log(result); // 3
 
 const result2 = [2,3,4,5,6,7,2,3,4,5]
-  findIndex(element => element % 2 !== 0);
+  .findIndex(element => element % 2 !== 0);
 console.log(result2); // 1
