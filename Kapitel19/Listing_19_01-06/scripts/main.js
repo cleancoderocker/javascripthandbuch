18127bd1c3677106c380436930c5d0daff1d38e4
Philip Ackermann <info@philipackermann.de>
2025-11-05 15:05:51 +0100

    Remove old chapter 19 code

--- Kapitel19/Listing_19_01-06/scripts/main.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-const os = require('os');
-document.addEventListener('DOMContentLoaded', () => {
-  const platform = os.platform();
-  document.getElementById('platform').textContent = platform;
-});
